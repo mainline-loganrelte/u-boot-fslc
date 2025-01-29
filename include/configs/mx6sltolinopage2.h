2f4c66378f8959125b2e2b3ffffe344061fd71e1
User0 <user0thenyancat@proton.me>
2025-01-30 06:23:35 +1300

    preliminary tolino page 2 support

---
+++ include/configs/mx6sltolinopage2.h
@@ -0,0 +1,72 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * based on works in mx6sltolinoshine3 by
+ * Copyright 2021 Andreas Kemnade
+ *
+ * Configuration settings for the Tolino Page 2 board.
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include "mx6_common.h"
+
+#if 0
+#define CONFIG_MXC_UART
+#endif
+#define CFG_MXC_UART_BASE		UART1_BASE
+
+/* I2C Configs */
+#ifdef CONFIG_CMD_I2C
+#define CONFIG_SYS_I2C_MXC
+#define CONFIG_SYS_I2C_MXC_I2C1		/* enable I2C bus 1 */
+#define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
+#define CONFIG_SYS_I2C_MXC_I2C3		/* enable I2C bus 3 */
+#define CONFIG_SYS_I2C_SPEED		  100000
+#endif
+#ifndef CONFIG_SPL_BUILD
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0) \
+	func(MMC, mmc, 1)
+
+#include <config_distro_bootcmd.h>
+#endif
+
+#define CFG_EXTRA_ENV_SETTINGS \
+	BOOTENV \
+	"console=ttymxc0\0" \
+	"fdt_addr_r=0x83000000\0" \
+	"kernel_addr_r=0x82000000\0" \
+	"loadaddr=0x82000000\0" \
+	"ramdisk_addr_r=0x83100000\0" \
+	"scriptaddr=0x87000000\0" \
+	"initrd_high=0xffffffff\0" \
+	"fdt_file=imx6sl-tolino-page2.dtb\0"
+
+/* Physical Memory Map */
+#define PHYS_SDRAM			MMDC0_ARB_BASE_ADDR
+#define PHYS_SDRAM_SIZE			SZ_512M
+
+#define CFG_SYS_SDRAM_BASE		PHYS_SDRAM
+#define CFG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
+#define CFG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+
+#define CONFIG_MMCROOT			"/dev/mmcblk0p2"  /* USDHC1 */
+
+#define CONFIG_CMDLINE_TAG                      /* enable passing of ATAGs */
+#define CONFIG_SETUP_MEMORY_TAGS
+#define CONFIG_INITRD_TAG
+#define CONFIG_REVISION_TAG
+
+/* MMC Configs */
+#define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC2_BASE_ADDR
+#define CONFIG_SYS_FSL_USDHC_NUM	3
+#define CONFIG_SYS_MMC_ENV_DEV		0  /* USDHC2 */
+
+/* USB Configs */
+#ifdef CONFIG_USB
+#define CONFIG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
+#endif
+
+#include <linux/stringify.h>
+#endif				/* __CONFIG_H */
